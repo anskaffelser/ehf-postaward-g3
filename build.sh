ff8c3f46d89ed91ddd338bb9d358fce775329afd
Erlend Klakegg Bergheim <erlend@klakegg.net>
2018-08-22 22:59:32 +0200

    Adding some code lists.

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ docker run --rm -i -v $PROJECT:/src difi/vefa-validator build -x -t -n no.difi.e
 echo
 
 # Guides
-docker run --rm -i -v $PROJECT:/documents -v $PROJECT/target/site:/target difi/asciidoctor
+docker run --rm -i -v $PROJECT/guide:/documents/guide -v $PROJECT/target/site:/target difi/asciidoctor
 
 # Fix ownership
 docker run --rm -i -v $PROJECT:/src alpine:3.6 chown -R $(id -g $USER).$(id -g $USER) /src/target
