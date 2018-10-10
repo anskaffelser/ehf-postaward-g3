3716a721084e7d1cbd634d6b2e2a1e363a9ca02c
Erlend Klakegg Bergheim <erlend@klakegg.net>
2018-10-10 14:17:46 +0200

    Adding some more documentation.

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@
 PROJECT=$(dirname $(readlink -f "$0"))
 
 if [ -e $PROJECT/target ]; then
-    docker run --rm -i -v $PROJECT:/src alpine:3.6 rm -rf /src/target
+    docker run --rm -i \
+        -v $PROJECT:/src \
+        alpine:3.6 \
+        rm -rf /src/target
 fi
 
 # Structure
@@ -15,12 +18,27 @@ docker run --rm -i \
 echo
 
 # Validator
-docker run --rm -i -v $PROJECT:/src difi/vefa-validator build -x -t -n no.difi.ehf.reminder.v2 -a rules -target target/validator /src
+docker run --rm -i \
+    -v $PROJECT:/src \
+    difi/vefa-validator \
+    build -x -t -n no.difi.ehf.postaward.g3 -a rules -target target/validator /src
 
 echo
 
 # Guides
-docker run --rm -i -v $PROJECT/guide:/documents/guide -v $PROJECT/target/site:/target difi/asciidoctor
+docker run --rm -i \
+    -v $PROJECT/guide:/documents/guide \
+    -v $PROJECT/target/site:/target \
+    difi/asciidoctor
+
+# Move release documentation
+docker run --rm -i \
+    -v $PROJECT/target/site:/src \
+    alpine:3.6 \
+    mv /src/guide/release /src/release
 
 # Fix ownership
-docker run --rm -i -v $PROJECT:/src alpine:3.6 chown -R $(id -g $USER).$(id -g $USER) /src/target
+docker run --rm -i \
+    -v $PROJECT:/src \
+    alpine:3.6 \
+    chown -R $(id -g $USER).$(id -g $USER) /src/target
